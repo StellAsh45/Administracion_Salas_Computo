4ca94bd7e88b7964c7d5a4cc28d8bc84d478b104
innmajo <maochebarrera@gmail.com>
2025-11-17 12:48:16 -0500

    Actualización Vista Admin

--- Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
+++ Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d67272b3ae076237464700213006f5b3ef69acf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ccc7721b7c886a1d9489b5b7645fdda6071782e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
