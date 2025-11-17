384d65d6dc5c0387171b02cb098247ab327d9ff9
juanda28 <davidalegria200502@gmail.com>
2025-11-17 15:15:32 -0500

    Estilos verEquipos

--- Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
+++ Web/MvcSample/obj/Debug/net9.0/MvcSample.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1ba09f93700b43095d7e47ece200fdbd6e94a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c6d2024ed467ef084ac3c970b3ac732ef1cde3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
