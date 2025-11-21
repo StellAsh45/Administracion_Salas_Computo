0326288794ec33a751d902c037bef88d2fcb7bca
Stellash45 <mlucioguillen@gmail.com>
2025-11-21 00:57:12 -0500

    Leves ajustes

--- Domain/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ffa272f981bda588fc755ebf363863cac5e7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cf9528f33aefb8f0c66c57871b4eb2323381b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
